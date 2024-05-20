777455881eef16ccc7e76ba8d228aa5147fc3a73
Jona Garcia <jalexisagarcia.91@gmail.com>
2024-05-20 19:08:03 -0300

    styles

--- Client/src/pages/BerenjenaGame/PagePrimaryBerenjena/pagePrimaryBerenjena.jsx
+++ Client/src/pages/BerenjenaGame/PagePrimaryBerenjena/pagePrimaryBerenjena.jsx
@@ -1,26 +1,29 @@
 import { Link } from "react-router-dom";
-import styles from "./pagePrimaryBerenjena.module.css"
+import styles from "./pagePrimaryBerenjena.module.css";
+import welcomeTitle from "../../../assets/berenjena/welcomeTitle.png";
 
-const PagePrimaryBerenjena =()=>{
-    return (
-        <div className={styles.contain}>
-        <div className={styles.containOption}>
-  
-          <h1>Bienvenido al Berenjena</h1>
-          <div className={styles.option}>
-  
-            <Link to="/gameberenjenaIA" className={styles.link}>
-              • Jugar contra la IA 👤
-            </Link>
-            <Link to="/gameberenjenafriend" className={styles.link}>
-              • Jugar con un amigo 👥
-            </Link>
-            <Link to="/rulesofberenjena" className={styles.link}>• Reglas 📜</Link>
-            <Link to="/" className={styles.link}>• Inicio 📜</Link>
-          </div>
-  
+const PagePrimaryBerenjena = () => {
+  return (
+    <div className={styles.contain}>
+      <div className={styles.containOption}>
+      <img src={welcomeTitle} alt="" className={styles.welcomeImg} />
+        <div className={`${styles.option} no-text-select`}>
+          <Link to="/gameberenjenaIA" className={styles.link}>
+            VS CPU
+          </Link>
+          <Link to="/gameberenjenafriend" className={styles.link}>
+            Multiplayer
+          </Link>
+          <Link to="/rulesofberenjena" className={styles.link}>
+            Rules
+          </Link>
+          <Link to="/" className={styles.link}>
+            Exit{" "}
+          </Link>
         </div>
       </div>
-    )
-}
+    </div>
+  );
+};
+
 export default PagePrimaryBerenjena;
--- Client/src/pages/BerenjenaGame/rulesBerenjena/rulesBerenjena.jsx
+++ Client/src/pages/BerenjenaGame/rulesBerenjena/rulesBerenjena.jsx
@@ -1,5 +1,5 @@
-import {Link} from 'react-router-dom';
-import style from './rulesBerenjena.module.css';
+import { Link } from "react-router-dom";
+import style from "./rulesBerenjena.module.css";
 
 const RulesOfBerenjena = () => {
   return (
@@ -8,44 +8,46 @@ const RulesOfBerenjena = () => {
         <h3>Reglas del juego</h3>
 
         <p>
-          El juego consiste en apostar una cierta cantidad de cartas acorde al número de repartidas y cumplir dicha apuesta.
+          El juego consiste en apostar una cierta cantidad de cartas acorde al
+          número de repartidas y cumplir dicha apuesta.
         </p>
-        <p style={{textDecoration: 'underline'}}>¿Como gano? </p>
+        <p style={{ textDecoration: "underline" }}>¿Como gano? </p>
         <p>
-          El juego comienza con 1 carta repartida y comienza el de la derecha al
-          {' '}
-          <span>Obligado</span>
-          {' '}
-          .
+          El juego comienza con 1 carta repartida y comienza el de la derecha al{" "}
+          <span>Obligado</span> .
         </p>
+        <p>El valor de las cartas, de mayor a menor son: 1,12,11... 3,2 .</p>
         <p>
-          El valor de las cartas, de mayor a menor son: 1,12,11... 3,2 .
-        </p>
-        <p>
-          Si tu rival de la izquierda tira una carta(ejemplo un 5) debes tirar un 6 o mayor ya que por mano gana la suya.
+          Si tu rival de la izquierda tira una carta(ejemplo un 5) debes tirar
+          un 6 o mayor ya que por mano gana la suya.
         </p>
 
         <p>Quien haya ganado la ronda comienza para la siguiente. </p>
         <p>
           <span>Obligado: </span>
-          
-           Es aquella persona que se ve obligada a romper la igualdad entre la cantidad de apuestas y cartas repartidas. ( ejemplo se reparten 3 cartas y la suma de la apuesta de cada jugador da 3, el obligado se ve "obligado" a apostar 1 o mas cartas, otro caso es si se reparten 3 cartas y la suma de apuestas da 2 entonces el obligado no puede decir 1 ya que igualaria los resultados por ende debe decir cero o dos).
+          Es aquella persona que se ve obligada a romper la igualdad entre la
+          cantidad de apuestas y cartas repartidas. ( ejemplo se reparten 3
+          cartas y la suma de la apuesta de cada jugador da 3, el obligado se ve
+          "obligado" a apostar 1 o mas cartas, otro caso es si se reparten 3
+          cartas y la suma de apuestas da 2 entonces el obligado no puede decir
+          1 ya que igualaria los resultados por ende debe decir cero o dos).
         </p>
         <p>
-          Las apuestas son por turnos comenzando desde la derecha del obligado en sentido antihorario, es decir que el "obligado" es el ultimo en apostar.
+          Las apuestas son por turnos comenzando desde la derecha del obligado
+          en sentido antihorario, es decir que el "obligado" es el ultimo en
+          apostar.
         </p>
         <p>
-          A la hora del puntaje quien cumpla, es decir, ganó las cartas que apostó se le sumará 5 puntos + la cantidad de cartas que ganó. (si cumplió pero fue 0 cartas solo sumaria 5 puntos ).
+          A la hora del puntaje quien cumpla, es decir, ganó las cartas que
+          apostó se le sumará 5 puntos + la cantidad de cartas que ganó. (si
+          cumplió pero fue 0 cartas solo sumaria 5 puntos ).
         </p>
         <div className={style.linkBackMenu}>
-
-          <Link to="/" className={style.link}>
-
+          <Link to="/berenjena" className={style.link}>
             ⬅ Menu
           </Link>
         </div>
       </div>
-
     </div>
   );
 };
