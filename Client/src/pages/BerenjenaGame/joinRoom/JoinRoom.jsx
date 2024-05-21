5e109b7239faa4f0b22dc7cfff3541a4350b9954
ignacio-nicoletti <nicolettiignacio5@gmail.com>
2024-05-21 14:39:32 -0300

    updates

--- Client/src/pages/BerenjenaGame/joinRoom/JoinRoom.jsx
+++ Client/src/pages/BerenjenaGame/joinRoom/JoinRoom.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import {useEffect, useState} from 'react';
 import style from './joinRoom.module.css';
 import {
   getAllRoomsInfo,
@@ -8,76 +8,82 @@ import {
 } from '../../../functions/SocketIO/sockets/sockets';
 import avatar from '../../../assets/berenjena/jugadores/avatar.png';
 import people from '../../../assets/berenjena/jugadores/people.png';
-import { Link, useNavigate } from 'react-router-dom';
+import {Link, useNavigate} from 'react-router-dom';
 
-const JoinRoom = ({ setRoomIdberenjena, roomIdberenjena }) => {
-  const [rooms, setRooms] = useState([]); //mapeo de todas las salas
-  const [game, setGame] = useState('Berenjena'); // Juego seleccionado
-  const [userName, setUserName] = useState(''); //mi nombre
-  const [roomId, setRoomId] = useState(''); //id de room
+const JoinRoom = ({setRoomIdberenjena, roomIdberenjena}) => {
+  const [rooms, setRooms] = useState ([]); //mapeo de todas las salas
+  const [game, setGame] = useState ('Berenjena'); // Juego seleccionado
+  const [userName, setUserName] = useState (''); //mi nombre
+  const [roomId, setRoomId] = useState (''); //id de room
 
-  const navigate = useNavigate();
+  const navigate = useNavigate ();
 
-  const CreateRoom = async (e) => {
-    e.preventDefault();
+  const CreateRoom = async e => {
+    e.preventDefault ();
 
     try {
       if (roomId !== '' && userName !== '') {
-        const response = await CreateGameRoom(game, roomId, userName);
-        console.log(response);
+        const response = await CreateGameRoom (game, roomId, userName);
+        console.log (response);
         //poner alerta de que uniste
       }
     } catch (error) {
-      console.error(error);
-      alert(error); // Muestra una alerta con el mensaje de error
+      console.error (error);
+      alert (error); // Muestra una alerta con el mensaje de error
     }
   };
 
-  const handlerJoinRoom = async (roomId) => {
+  const handlerJoinRoom = async roomId => {
     try {
       if (roomId !== '' && userName !== '') {
-        const response = await joinGameRoom(game, roomId, userName);
-        console.log(response);
+        const response = await joinGameRoom (game, roomId, userName);
+        console.log (response);
       }
     } catch (error) {
-      console.error(error);
-      alert(error); // Muestra una alerta con el mensaje de error
+      console.error (error);
+      alert (error); // Muestra una alerta con el mensaje de error
     }
   };
 
-  useEffect(() => {
-    const initializeSocket = async () => {
-      const roomsInfo = await getAllRoomsInfo(game);
-      setRooms(roomsInfo);
-    };
-    initializeSocket();
-  }, [game]);
+  useEffect (
+    () => {
+      const initializeSocket = async () => {
+        const roomsInfo = await getAllRoomsInfo (game);
+        setRooms (roomsInfo);
+      };
+      initializeSocket ();
+    },
+    [game]
+  );
 
-  useEffect(() => {
-    const handlePlayerList = (playerList) => {
-      console.log(playerList);
-      navigate('/berenjena/multiplayer');
-    };
+  useEffect (
+    () => {
+      const handlePlayerList = playerList => {
+        console.log (playerList);
+        navigate ('/berenjena/multiplayer');
+      };
 
-    socket.on('player_list', handlePlayerList);
+      socket.on ('player_list', handlePlayerList);
 
-    return () => {
-      socket.off('player_list', handlePlayerList);
-    };
-  }, [navigate]);
+      return () => {
+        socket.off ('player_list', handlePlayerList);
+      };
+    },
+    [navigate]
+  );
 
-  useEffect(() => {
-    const handlePosition = (position) => {
-      setRoomIdberenjena((prev) => ({ ...prev, positionId: position }));
-      console.log(position);
-    };
-
-    socket.on('position', handlePosition);
-
-    return () => {
-      socket.off('position', handlePosition);
-    };
-  }, [setRoomIdberenjena]);
+  useEffect (
+    () => {
+      const handlePosition = position => {
+        setRoomIdberenjena (prev => ({...prev, positionId: position}));
+      };
+      socket.on ('position', handlePosition);
+      return () => {
+        socket.off ('position', handlePosition);
+      };
+    },
+    [setRoomIdberenjena]
+  );
 
   return (
     <div className={style.containRoom}>
@@ -93,14 +99,14 @@ const JoinRoom = ({ setRoomIdberenjena, roomIdberenjena }) => {
             type="text"
             placeholder="Name"
             value={userName}
-            onChange={(e) => setUserName(e.target.value)}
+            onChange={e => setUserName (e.target.value)}
             maxLength={15}
           />
         </div>
         <div className={style.DivAvatars}>
           <p>Choose your Avatar</p>
           <div className={style.DivAvatarsGrid}>
-            {[...Array(6)].map((_, index) => (
+            {[...Array (6)].map ((_, index) => (
               <img key={index} src={avatar} alt="" />
             ))}
           </div>
@@ -111,21 +117,21 @@ const JoinRoom = ({ setRoomIdberenjena, roomIdberenjena }) => {
             type="number"
             placeholder="Type number of Room "
             value={roomId}
-            onChange={(e) => setRoomId(e.target.value)}
+            onChange={e => setRoomId (e.target.value)}
           />
           <button onClick={CreateRoom}>Create</button>
         </div>
       </div>
 
       <div className={style.DivRooms}>
-        {rooms.map((el) => (
+        {rooms.map (el => (
           <div key={el.roomId} className={style.DivRoom}>
             <p>sala {el.roomId}</p>
             <img src={people} alt="" />
             <p>{el.users.length}/6</p>
             <button
               onClick={() => {
-                handlerJoinRoom(el.roomId);
+                handlerJoinRoom (el.roomId);
               }}
             >
               Join
