cbfbee49f929cafb9d03715ecdb8245c390b1c12
Jona Garcia <jalexisagarcia.91@gmail.com>
2024-05-27 15:25:29 -0300

    styles

--- Client/src/components/berenjena/loader/loader.jsx
+++ Client/src/components/berenjena/loader/loader.jsx
@@ -1,9 +1,9 @@
-import style from './loader.module.css';
-import ButtonExitRoom from '../buttonExitRoom/buttonExitRoom';
-import { useEffect, useState } from 'react';
-import { socket } from '../../../functions/SocketIO/sockets/sockets';
-import { useParams } from 'react-router-dom';
-import PersonIcon from '@mui/icons-material/Person';
+import style from "./loader.module.css";
+import ButtonExitRoom from "../buttonExitRoom/buttonExitRoom";
+import { useEffect, useState } from "react";
+import { socket } from "../../../functions/SocketIO/sockets/sockets";
+import { useParams } from "react-router-dom";
+import PersonIcon from "@mui/icons-material/Person";
 
 const Loader = ({ game, setMyPosition, players, setPlayers }) => {
   const [readyMe, setReadyMe] = useState(false);
@@ -13,61 +13,64 @@ const Loader = ({ game, setMyPosition, players, setPlayers }) => {
   const handleReady = () => {
     setReadyMe(true);
 
-    socket.emit('player_ready', {
+    socket.emit("player_ready", {
       game,
       roomId: id,
     });
   };
 
   useEffect(() => {
-    socket.on('player_list', data => {
+    socket.on("player_list", (data) => {
       setPlayerList(data);
 
-      const currentPlayer = data.find(player => player.id === socket.id);
+      const currentPlayer = data.find((player) => player.id === socket.id);
       if (currentPlayer) {
         setMyPosition(currentPlayer);
       }
 
       // Update the players state with the received data
-      setPlayers(prevPlayers =>
-        prevPlayers.map(player => {
-          const updatedPlayer = data.find(p => p.position === player.id);
-          return updatedPlayer ? { ...player, userName: updatedPlayer.userName } : player;
+      setPlayers((prevPlayers) =>
+        prevPlayers.map((player) => {
+          const updatedPlayer = data.find((p) => p.position === player.id);
+          return updatedPlayer
+            ? { ...player, userName: updatedPlayer.userName }
+            : player;
         })
       );
     });
 
     return () => {
-      socket.off('player_list');
+      socket.off("player_list");
     };
   }, []);
 
   useEffect(() => {
     // Escuchar el evento cuando se une un nuevo jugador
-    socket.on('room_joined', data => {
+    socket.on("room_joined", (data) => {
       setMyPosition(data);
-      setPlayers(prevPlayers =>
-        prevPlayers.map(player =>
-          player.id === data.position ? { ...player, userName: data.userName } : player
+      setPlayers((prevPlayers) =>
+        prevPlayers.map((player) =>
+          player.id === data.position
+            ? { ...player, userName: data.userName }
+            : player
         )
       );
     });
 
     // Escuchar el evento cuando un jugador está listo
-    socket.on('player_ready_status', playerReadyStatus => {
-      setPlayerList(prevList =>
-        prevList.map(
-          player =>
-            player.id === playerReadyStatus.id
-              ? { ...player, ready: playerReadyStatus.ready }
-              : player
+    socket.on("player_ready_status", (playerReadyStatus) => {
+      setPlayerList((prevList) =>
+        prevList.map((player) =>
+          player.id === playerReadyStatus.id
+            ? { ...player, ready: playerReadyStatus.ready }
+            : player
         )
       );
     });
 
     return () => {
-      socket.off('room_joined');
-      socket.off('player_ready_status');
+      socket.off("room_joined");
+      socket.off("player_ready_status");
     };
   }, []);
 
@@ -77,22 +80,22 @@ const Loader = ({ game, setMyPosition, players, setPlayers }) => {
       <div className={style.playersAndButton}>
         <div className={style.PlayersReady}>
           {playerList.map((player, index) => (
-            <img
-              key={index}
-              src={player.imageUrl} // Aquí se asume que cada jugador tiene una propiedad imageUrl con la URL de su imagen
-              alt={`Player ${index + 1}`}
-              className={style.playerImage}
-              style={{ borderColor: player.ready ? '#ff904f' : '#dededf' }}
-            />
-          ))}
-          {/* Agregar una imagen adicional si el usuario actual no está en la lista de jugadores */}
-          {!playerList.some(player => player.id === socket.id) &&
             <PersonIcon
               sx={{ fontSize: 50 }}
               alt="Current Player"
               className={style.playerImage}
-              style={{ borderColor: readyMe ? '#ff904f' : '#dededf' }}
-            />}
+              style={{ borderColor: readyMe ? "#ff904f" : "#dededf" }}
+            />
+          ))}
+
+          {!playerList.some((player) => player.id === socket.id) && (
+            <PersonIcon
+              sx={{ fontSize: 50 }}
+              alt="Current Player"
+              className={style.playerImage}
+              style={{ borderColor: readyMe ? "#ff904f" : "#dededf" }}
+            />
+          )}
         </div>
         <div>
           <button
