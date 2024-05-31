bdac878f7331c20ead903bc0cc23b8f91fc24176
Jona Garcia <jalexisagarcia.91@gmail.com>
2024-05-31 17:06:03 -0300

    styles

--- Client/src/pages/BerenjenaGame/Berenjena/berenjena.jsx
+++ Client/src/pages/BerenjenaGame/Berenjena/berenjena.jsx
@@ -1,29 +1,25 @@
-import {useEffect, useState} from 'react';
+import { useEffect, useState } from 'react';
 import style from './berenjena.module.css';
 import Jugadores from '../../../components/berenjena/jugadores/jugadores';
 import Loader from '../../../components/berenjena/loader/loader';
-import DataGame
-  from '../../../components/berenjena/informationGame/dataGame/dataGame';
-import DataPlayer
-  from '../../../components/berenjena/informationGame/dataPlayer/dataPlayer';
-import Result
-  from '../../../components/berenjena/informationGame/Results/result';
-import {distribute} from '../../../functions/logica/logica.js';
+import DataGame from '../../../components/berenjena/informationGame/dataGame/dataGame';
+import DataPlayer from '../../../components/berenjena/informationGame/dataPlayer/dataPlayer';
+import Result from '../../../components/berenjena/informationGame/Results/result';
+import { distribute } from '../../../functions/logica/logica.js';
 import Apuesta from '../../../components/berenjena/apuesta/apuesta';
-import ButtonExitRoom
-  from '../../../components/berenjena/buttonExitRoom/buttonExitRoom.jsx';
-import {socket} from '../../../functions/SocketIO/sockets/sockets';
+import ButtonExitRoom from '../../../components/berenjena/buttonExitRoom/buttonExitRoom.jsx';
+import { socket } from '../../../functions/SocketIO/sockets/sockets';
 import MyCards from '../../../components/berenjena/myCards/myCards.jsx';
 
 const GameBerenjena = () => {
-  const [loader, setLoader] = useState (true);
-  const [game, setGame] = useState ('Berenjena'); // Juego seleccionado
-  const [showResult, setShowResult] = useState (false);
-  const [myPosition, setMyPosition] = useState (1);
+  const [loader, setLoader] = useState(true);
+  const [game, setGame] = useState('Berenjena'); // Juego seleccionado
+  const [showResult, setShowResult] = useState(false);
+  const [myPosition, setMyPosition] = useState(1);
 
-  const [players, setPlayers] = useState ([]);
+  const [players, setPlayers] = useState([]);
 
-  const [round, setRound] = useState ({
+  const [round, setRound] = useState({
     users: null, //usuarios conectados
     numRounds: 0, //num de ronda
     hands: 0, //igual a cant de cards repartidas
@@ -43,20 +39,12 @@ const GameBerenjena = () => {
     timer: '',
   });
 
-  const [Base, setBase] = useState ([]); //base del resultado xronda
+  const [Base, setBase] = useState([]); //base del resultado xronda
 
-  //avisos: Obligado; turno de quien
-  //el select apuesta si no clickeo me pone por default
-  //error en joinroom testear bien string y filtrado
-
-
-// css: berenjena, jugadores ,card
-
-
-  useEffect (() => {
+  useEffect(() => {
     const handleStartGame = data => {
-      setLoader (prevLoader => !prevLoader);
-      setRound ({
+      setLoader(prevLoader => !prevLoader);
+      setRound({
         ...round,
         typeRound: 'Bet',
         turnJugadorA: data.nextTurn + 1,
@@ -72,52 +60,44 @@ const GameBerenjena = () => {
       });
     };
 
-    socket.on ('start_game', handleStartGame);
+    socket.on('start_game', handleStartGame);
 
     return () => {
-      socket.off ('start_game', handleStartGame);
+      socket.off('start_game', handleStartGame);
     };
   }, []);
 
-  useEffect (
-    () => {
-      if (round.typeRound === 'Bet') {
-        distribute (round, setPlayers, players);
-      }
-    },
-    [round.typeRound]
-  );
+  useEffect(() => {
+    if (round.typeRound === 'Bet') {
+      distribute(round, setPlayers, players);
+    }
+  }, [round.typeRound]);
+
+  const renderPlayers = () => {
+    const positions = ['jugador2', 'jugador3', 'jugador4', 'jugador5', 'jugador6'];
+    const filteredPlayers = players.filter((_, index) => index !== myPosition - 1);
+    const reorderedPlayers = [...filteredPlayers.slice(myPosition - 1), ...filteredPlayers.slice(0, myPosition - 1)];
+
+    return reorderedPlayers.slice(0, positions.length).map((player, index) => (
+      <div className={style[positions[index]]} key={index}>
+        <Jugadores player={player} />
+      </div>
+    ));
+  };
 
   return (
     <div className={style.contain}>
-
-      {loader == true
-        ? <Loader
-            game={game}
-            setMyPosition={setMyPosition}
-            setPlayers={setPlayers}
-          />
-        : <div className={style.tableroJugadores}>
-
-            <div className={style.jugador2}>
-              <Jugadores player={players[1]} />
-            </div>
-            <div className={style.jugador3}>
-              <Jugadores player={players[1]} />
-            </div>
-            <div className={style.jugador4}>
-              <Jugadores player={players[1]} />
-            </div>
-            <div className={style.jugador5}>
-              <Jugadores player={players[1]} />
-            </div>
-            <div className={style.jugador6}>
-              <Jugadores player={players[1]} />
-            </div>
-
-          </div>}
-      <div />
-
+      {loader ? (
+        <Loader
+          game={game}
+          setMyPosition={setMyPosition}
+          setPlayers={setPlayers}
+        />
+      ) : (
+        <div className={style.tableroJugadores}>
+          {renderPlayers()}
+        </div>
+      )}
       <MyCards
         myPosition={myPosition}
         players={players}
@@ -125,30 +105,26 @@ const GameBerenjena = () => {
         setRound={setRound}
         round={round}
       />
-
       <DataPlayer myPosition={myPosition} players={players} />
-
-      {round.typeRound === 'Bet'
-        ? <Apuesta
-            players={players}
-            setPlayers={setPlayers}
-            round={round}
-            setRound={setRound}
-            myPosition={myPosition}
-          />
-        : ''}
-
+      {round.typeRound === 'Bet' && (
+        <Apuesta
+          players={players}
+          setPlayers={setPlayers}
+          round={round}
+          setRound={setRound}
+          myPosition={myPosition}
+        />
+      )}
       <DataGame round={round} />
-      {showResult === true
-        ? <Result
-            setShowResult={setShowResult}
-            players={players}
-            round={round}
-          />
-        : ''}
+      {showResult && (
+        <Result
+          setShowResult={setShowResult}
+          players={players}
+          round={round}
+        />
+      )}
       <ButtonExitRoom />
-
-      <div className={style.resultado} onClick={() => setShowResult (true)}>
+      <div className={style.resultado} onClick={() => setShowResult(true)}>
         <p>Resultados</p>
       </div>
     </div>
