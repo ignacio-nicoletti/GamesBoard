ac6613598a6df2840208a37a376df0c9f5f45d4a
Jona Garcia <jalexisagarcia.91@gmail.com>
2024-06-05 18:13:02 -0300

    styles

--- Client/src/components/berenjena/informationGame/dataGame/dataGame.jsx
+++ Client/src/components/berenjena/informationGame/dataGame/dataGame.jsx
@@ -1,28 +1,35 @@
-import style from "./dataGame.module.css"
-const DataGame=({round})=>{
-// console.log(round);
- 
-    return(
-        <div className={style.infoPartida}>
-              <p>tipo: {round?.typeRound} </p>
-              <p>Obligado: jugador{round?.obligado}</p>
-       
-              <p>Apuesta total: {round?.betTotal}</p>
-              <p>
-                Carta Ganadora:
+import React from "react";
+import style from "./dataGame.module.css";
 
-                {' ' + round?.cardWinxRound?.value + ' '}
+const DataGame = ({ round }) => {
+  return (
+    <div className={style.infoPartida}>
+      <p>
+        Obligado: <span className={style.dataValue}>{round?.obligado}</span>
+      </p>
+      <p>
+        Apuesta Total:{" "}
+        <span className={style.dataValue}>{round?.betTotal}</span>
+      </p>
+      <p>
+        Carta Ganadora:{" "}
+        <span className={style.dataValue}>
+          {round?.cardWinxRound?.value} {round?.cardWinxRound?.suit}
+        </span>
+      </p>
+      <p>
+        Mano: <span className={style.dataValue}>{round?.hands}</span>
+      </p>
+      <p>
+        Turno:{" "}
+        <span className={style.dataValue}>
+          {round?.typeRound === "Bet"
+            ? round?.turnJugadorA
+            : round?.turnJugadorR}
+        </span>
+      </p>
+    </div>
+  );
+};
 
-                {round?.cardWinxRound?.suit}
-              </p>
-              <p>hand: {round?.hands}</p>
-              <p>
-                turno: jugador
-                {round?.typeRound === 'Bet'
-                  ? round?.turnJugadorA
-                  : round?.turnJugadorR}
-              </p>
-            </div>
-    )
-}
 export default DataGame;
