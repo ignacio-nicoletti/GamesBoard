3d9651ca06c5c6ea60729a5409f740f5b6dc209d
ignacio-nicoletti <nicolettiignacio5@gmail.com>
2024-07-19 19:36:32 -0300

    updates

--- Client/src/functions/SocketIO/sockets/sockets.js
+++ Client/src/functions/SocketIO/sockets/sockets.js
@@ -3,8 +3,10 @@ import { io } from "socket.io-client";
 const URL =
   process.env.REACT_APP_NODE_ENV === "production"
     ? process.env.REACT_APP_URL_API
-    : process.env.REACT_APP_NODE_ENV === "develoment" ?
-      process.env.REACT_APP_URL_API_LOCAL:"";
+    : process.env.REACT_APP_NODE_ENV === "development"
+    ? process.env.REACT_APP_URL_API_LOCAL
+    : "";
+
 // Se conecta al servidor
 
 export let socket = io(URL, {
